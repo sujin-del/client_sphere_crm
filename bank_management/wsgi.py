ef16a62740d141da5599ba8e6d0003133c9bf029
Stevin <sjstevinramesh@gmail.com>
2026-05-01 19:34:19 +0530

    created django bank_management project and added the base template for the CRM

--- bank_management/wsgi.py
+++ bank_management/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for crudproject project.
+WSGI config for bank_management project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crudproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bank_management.settings')
 
 application = get_wsgi_application()
--- crudapp/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Orders
-
-class OrdersSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Orders
-        fields = ['oid', 'fname', 'lname', 'price', 'mail', 'addr']
