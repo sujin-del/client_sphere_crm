46bd9a2ebb3069df78d673ea9b7ba95ec9349460
Abinaya-mk <abinaya.mk06@gmail.com>
2026-05-05 13:56:13 +0530

    task_statuses is created

--- Task/Task/taskapp/models.py
+++ Task/Task/taskapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class AccountStatus(models.Model):
+    name = models.CharField(max_length=45)
+
+    class Meta:
+        db_table = "account_statuses"
+
+    def __str__(self):
+        return self.name
