4ddcaae4153a05d370d3cfa366b9c94b42452b69
ananshi10 <jananshi73@gmail.com>
2026-05-05 15:58:34 +0530

    TaskCategory

--- Task/Task/taskapp/views.py
+++ Task/Task/taskapp/views.py
@@ -35,3 +35,30 @@ def task_priority_delete(request, pk):
     priority = get_object_or_404(TaskPriority, pk=pk)
     priority.delete()
     return redirect('task_priority_list')
+
+from django.shortcuts import render, redirect
+from .models import TaskCategory
+
+def category_list(request):
+    categories = TaskCategory.objects.all()
+    return render(request, "task_categories/list.html", {"categories": categories})
+
+def add_category(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        parent_id = request.POST.get("parent_id", 0)
+        sort_order_index = request.POST.get("sort_order_index", None)
+
+        TaskCategory.objects.create(
+            name=name,
+            parent_id=parent_id,
+            sort_order_index=sort_order_index
+        )
+        return redirect("category_list")
+
+    return render(request, "task_categories/add.html")
+
+def delete_category(request, category_id):
+    category = TaskCategory.objects.get(id=category_id)
+    category.delete()
+    return redirect("category_list")
