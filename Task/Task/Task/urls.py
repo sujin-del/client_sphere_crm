8f8262c75763d72340b6c4b9ff1cae7090934219
Arthi-as <arthi.ai24@stellamaryscoe.edu.in>
2026-05-06 09:45:25 +0530

    urls.py edited

--- Task/Task/Task/urls.py
+++ Task/Task/Task/urls.py
@@ -1,17 +1,7 @@
-from django.urls import path
-from . import views
-
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('frequency-types/', views.frequency_list, name='frequency_list'), #taskfrequency
-]
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("categories/", views.category_list, name="category_list"),
-    path("categories/add/", views.add_category, name="add_category"),
-    path("categories/delete/<int:category_id>/", views.delete_category, name="delete_category"),
+    path('', include('taskapp.urls')),  
 ]
--- Task/Task/taskapp/urls.py
+++ Task/Task/taskapp/urls.py
@@ -1,8 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('', views.task_priority_list, name='task_priority_list'),
-    path('update/<int:pk>/', views.task_priority_update, name='task_priority_update'),
-    path('delete/<int:pk>/', views.task_priority_delete, name='task_priority_delete'),
-]
+urlpatterns = [] 
