7963dab4b926a991b0989b7f28fab80d3efd3a7b
Arthi-as <arthi.ai24@stellamaryscoe.edu.in>
2026-05-06 09:38:19 +0530

    committed

--- Task/Task/Task/urls.py
+++ Task/Task/Task/urls.py
@@ -1,18 +1,10 @@
 from django.urls import path
 from . import views
-<<<<<<< HEAD
-
-urlpatterns = [
-    path("categories/", views.category_list, name="category_list"),
-    path("categories/add/", views.add_category, name="add_category"),
-    path("categories/delete/<int:category_id>/", views.delete_category, name="delete_category"),
-=======
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('frequency-types/', views.frequency_list, name='frequency_list'), #taskfrequency
->>>>>>> 9c64740adb8adbd349bd5cd396bc0b58c2c6222d
 ]
 
 from django.urls import path
