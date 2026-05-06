ad1051693be8c3fdb5a74ad285b121098a733381
shanaai24-create <shana.ai24@stellamaryscoe.edu.in>
2026-05-06 10:17:28 +0530

    Update views.py

--- organization/project_task/views.py
+++ organization/project_task/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .project_forms import ProjectForm
+from .project_models import Project
 
-# Create your views here.
+def project_create(request):
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('project_list')
+    else:
+        form = ProjectForm()
+    return render(request, 'project.html', {'form': form})
+
+def project_list(request):
+    projects = Project.objects.all()
+    return render(request, 'base.html', {'projects': projects})
