8728a6e22b5bb61c7001200ba8dbacef3346af3f
Arthi-as <arthi.ai24@stellamaryscoe.edu.in>
2026-05-06 10:06:18 +0530

    DB_Tables_created

--- Task/Task/Task/settings.py
+++ Task/Task/Task/settings.py
@@ -73,10 +73,19 @@ WSGI_APPLICATION = 'Task.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv(os.path.join(BASE_DIR.parent.parent, '.env'))
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '3306'),
     }
 }
 
